eed6f9b8b588a699eb0f8c22357831063d017f00
Smita Thube <smita.thube-v@adityabirlacapital.com>
2023-08-14 10:46:54 +0530

    added obser and operator on it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, find, first, from, interval, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  timer = from([1, 2, 3]);
 
+  ngOnInit() {
+    //let timer = from([1,2,3]); // creating strem of values called observable by rxjs
+    this.timer.subscribe((values) => console.log(values));
+
+    let myInternal = interval(1).pipe(
+      take(100),
+      map((val) => val *2),
+      find((value) => value > 99)
+    ); // emit strean if values // obser
+
+    myInternal.subscribe({
+      next: (success) => {
+        console.log( `I am out put of stream ==> ${success}`);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
