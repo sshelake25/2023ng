54e0a309ce9f56fd3ba25208e768060dc50b40ea
Smita Thube <smita.thube-v@adityabirlacapital.com>
2023-08-16 09:41:28 +0530

    bound lang options dynamically in html form

--- src/app/patient-info/patient-info.component.ts
+++ src/app/patient-info/patient-info.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface langCofi {
+  lang: string,
+  code: string,
+}
+
 @Component({
   selector: 'app-patient-info',
   templateUrl: './patient-info.component.html',
   styleUrls: ['./patient-info.component.css']
 })
 export class PatientInfoComponent {
-
+ public langs: langCofi[] = [
+   {lang: 'english', code: 'en-Us'},
+   {lang: 'german', code: 'jn-Us'},
+   {lang: 'hind', code: 'hn-Us'},
+   {lang: 'xly', code: 'xly-Us'}
+  ]
 }
